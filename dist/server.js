b1fe5dba59bc3ad0c1f3de40ae8759f0239c9280
Aleš Pros <alisek.pros@seznam.cz>
2017-02-27 15:37:53 +0100

    Finished

--- dist/server.js
+++ dist/server.js
@@ -3,41 +3,90 @@
 var express = require('express');
 var app = express();
 
-var ImagesClient = require('google-images');
+const Promise = require("bluebird");
 
-var client = new ImagesClient('CSE ID', 'API KEY');
+Promise.promisifyAll(require("mongodb"));
 
-client.search('Steve Angello').then(function (images) {
-    /*
-     [{
-     "url": "http://steveangello.com/boss.jpg",
-     "type": "image/jpeg",
-     "width": 1024,
-     "height": 768,
-     "size": 102451,
-     "thumbnail": {
-     "url": "http://steveangello.com/thumbnail.jpg",
-     "width": 512,
-     "height": 512
-     }
-     }]
-     */
+const mongo = require('mongodb')
+var MongoClient = mongo.MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017';
+// Use connect method to connect to the Server
+
+
+//var ImagesClient = require('google-images');
+ var ImagesClient = require("./googleimg")
+
+
+var CSEID = ""; // here put the CSE ID
+var APIKEY = ""; // here put the API KEY
+
+var client = new ImagesClient(CSEID, APIKEY);
+
+
+app.get('/api/imagesearch/:searchString', function (req, res) {
+
+    var searchString = req.params.searchString;
+    var offset = req.query.offset;
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+
+        var collection = db.collection('imagesearch')
+        try {
+            collection.insertOne({"term": searchString, "when": new Date()})
+        } catch(e) {
+            throw new Error("error during inserting to database", e)
+        }
+
+
+        db.close();
+    });
+
+    client.search(searchString, {
+        page: parseInt(offset)
+    }).then(function (images) {
+        res.send(images)
+    });
+
 });
 
-// paginate results
-client.search('Steve Angello', {
-    page: 2
-});
 
-// search for certain size
-client.search('Steve Angello', {
-    size: 'large'
-});
+app.get('/api/latest/imagesearch/', function (req, res) {
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        var collection = db.collection('imagesearch')
+
+        collection.find().sort({_id:-1}).limit(10).toArray(function(err, items) {
+            if (err) {
+                throw new Error("Error reading the database", err)
+            }
+            items.map(function (item) {
+                delete item["_id"]
+            })
+
+            res.send(items)
+            db.close();
+        });
+
+    });
+
+
+})
+
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send("Hello world!")
 });
 
 app.listen(3000, function () {
-    console.log('Running now!!!!');
+    console.log('Running now!');
 });
